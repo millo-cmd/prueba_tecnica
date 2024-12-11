94c14fd7b0d84cf38b8776fdfd6f51ba62044546
millo-cmd <3612emiliogonzalez@gmail.com>
2024-12-10 23:19:13 -0600

    cors

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const colaboradoresRoutes = require('./routes/coloboradoresRoutes');
 const usuariosRoutes = require('./routes/usuariosRoutes');
 
 const app = express();
+
 app.use(bodyParser.json());
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Permite solicitudes desde tu cliente React
+    methods: ['GET', 'POST', 'DELETE', 'PUT'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+  }));
 
 app.use('/colaboradores', colaboradoresRoutes);
 app.use('/usuarios', usuariosRoutes);
 
-const PORT = 3000;
+const PORT = 4000;
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
